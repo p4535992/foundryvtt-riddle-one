90513ef4b0d6ad9370bcd262294aeb626bfcb40a
Marco Tenti <marco.tenti@abd.it>
2021-11-17 17:50:52 +0100

    update

---
+++ src/module/Hooks.ts
@@ -0,0 +1,11 @@
+import { warn, error, debug, i18n } from '../foundryvtt-riddle-one';
+
+export const readyHooks = async () => {};
+
+export const setupHooks = async () => {
+  // setup all the hooks
+};
+
+export const initHooks = () => {
+  warn('Init Hooks processing');
+};
